a44dcf18fd8e6a1a23d41e35d6bb5800df03c1ea
TodkarPrem <premst95@gmail.com>
2020-05-28 23:16:38 +0530

    Delete duplicate nodes code added.
    Signed-off-by: TodkarPrem <premst95@gmail.com>

---
+++ dataStructures/deleteDuplicates.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct singleLL {
+	int num;
+	struct singleLL *next;
+} SLL;
+
+void addNode(SLL **hptr)
+{
+	int num;
+	SLL *ptr, *tmp;
+
+	printf("Enter integer value: ");
+	scanf("%d", &num);
+
+	ptr = (SLL *) malloc(sizeof(SLL));
+	ptr->num = num;
+
+	if (*hptr == 0) {
+		ptr->next = *hptr;
+		*hptr = ptr;
+	}
+	else {
+		tmp = *hptr;
+		while (tmp->next)
+			tmp = tmp->next;
+		ptr->next = 0;
+		tmp->next = ptr;
+	}
+}
+
+void print(SLL *ptr)
+{
+	while (ptr) {
+		printf("%d ", ptr->num);
+		ptr = ptr->next;
+	}
+	printf("\n");
+}
+
+void deleteDuplicates(SLL *ptr)
+{
+	SLL *tmp1 = ptr, *tmp2, *dup;
+
+	while (tmp1->next) {
+		tmp2 = tmp1->next;
+		while (tmp2) {
+			if (tmp1->num == tmp2->num) {
+				printf("Duplicate Number found.\n");
+				dup = tmp2;
+				tmp2 = tmp2->next;
+				free(dup);
+			}
+			else
+				tmp2 = tmp2->next;
+		}
+		tmp1 = tmp1->next;
+	}
+}
+
+
+int main()
+{
+	SLL *hp = 0;
+	char ch;
+
+	do {
+		addNode(&hp);
+		printf("want to continue(y/n): ");
+		scanf(" %c", &ch);
+	} while (ch == 'y' || ch == 'Y');
+
+	printf("Added nodes as it is: ");
+	print(hp);
+	deleteDuplicates(hp);
+	printf("Nodes after deleting nodes: ");
+	print(hp);
+
+	return 0;
+}
